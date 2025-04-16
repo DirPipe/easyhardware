856845a7e662c47a30186b362498e4f24ac0565c
DirPipe <luis.castano29545@ucaldas.edu.co>
2025-04-16 13:23:51 -0500

    inicializacion repositorio git, l, levantamiento de esquema y capas app

---
+++ SolucionSistemaVenta/SistemaVenta.IOC/Dependencia.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaVenta.DAL.DBContext;
+using Microsoft.EntityFrameworkCore;
+//using SistemaVenta.DAL.Interfaces;
+//using SistemaVenta.DAL.Implementacion;
+//using SistemaVenta.BLL.Interfaces;
+//using SistemaVenta.BLL.Implementacion;
+
+namespace SistemaVenta.IOC
+{
+    public static class Dependencia
+    {
+        public static void InyectarDependencia(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Configuración de la cadena de conexión
+            services.AddDbContext<DbventaContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("CadenaSQL")));
+
+            // Inyección de dependencias para los repositorios y servicios
+            //services.AddScoped<IProductoRepository, ProductoRepository>();
+            //services.AddScoped<IProductoService, ProductoService>();
+            //services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+            //services.AddScoped<ICategoriaService, CategoriaService>();
+        }
+    }
+}
